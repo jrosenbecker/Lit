ea75fedb80513e8816c18dd966eed4b8e778beea
jrosenbecker <rosenbeckerjw@gmail.com>
2016-03-19 12:24:58 -0500

    Configured the fragments to display based on how the tab at the bottom of the screen is selected. Also forced the main activity to be a portrait orientation

---
+++ app/src/main/java/com/lit/Constants/TabConstants.java
@@ -0,0 +1,10 @@
+package com.lit.Constants;
+
+/**
+ * Created by JoeLaptop on 3/19/2016.
+ */
+public class TabConstants {
+    public static final int STATUS_TAB = 0;
+    public static final int POWER_SAVE_TAB = 1;
+    public static final int CUSTOMIZE_TAB = 2;
+}
--- app/src/main/java/com/lit/MainActivity.java
+++ app/src/main/java/com/lit/MainActivity.java
@@ -1,10 +1,19 @@
 package com.lit;
 
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lit.Constants.TabConstants;
+import com.lit.fragments.CustomizeFragment;
+import com.lit.fragments.PowerSaveFragment;
+import com.lit.fragments.StatusFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements CustomizeFragment.OnFragmentInteractionListener,
+        PowerSaveFragment.OnFragmentInteractionListener,
+        StatusFragment.OnFragmentInteractionListener {
 
     TabLayout tabLayout;
 
@@ -14,8 +23,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tabLayout = (TabLayout) findViewById(R.id.tabMenu);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-        tabLayout.addTab(tabLayout.newTab().setText("Status"));
-        tabLayout.addTab(tabLayout.newTab().setText("Power Save"));
-        tabLayout.addTab(tabLayout.newTab().setText("Customize"));
+        tabLayout.addTab(tabLayout.newTab().setText("Status").setTag(TabConstants.STATUS_TAB));
+        tabLayout.addTab(tabLayout.newTab().setText("Power Save").setTag(TabConstants.POWER_SAVE_TAB));
+        tabLayout.addTab(tabLayout.newTab().setText("Customize").setTag(TabConstants.CUSTOMIZE_TAB));
+        tabLayout.setOnTabSelectedListener(onTabSelected);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_frame, StatusFragment.newInstance()).commit();
+
+    }
+
+    private TabLayout.OnTabSelectedListener onTabSelected = new TabLayout.OnTabSelectedListener() {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            switch((int) tab.getTag())
+            {
+                case TabConstants.STATUS_TAB:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, StatusFragment.newInstance()).commit();
+                    break;
+                case TabConstants.POWER_SAVE_TAB:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, PowerSaveFragment.newInstance()).commit();
+                    break;
+                case TabConstants.CUSTOMIZE_TAB:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, CustomizeFragment.newInstance()).commit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+            switch((int) tab.getTag())
+            {
+                case TabConstants.STATUS_TAB:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, StatusFragment.newInstance()).commit();
+                    break;
+                case TabConstants.POWER_SAVE_TAB:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, PowerSaveFragment.newInstance()).commit();
+                    break;
+                case TabConstants.CUSTOMIZE_TAB:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, CustomizeFragment.newInstance()).commit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+    };
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // Blank listener
     }
 }
