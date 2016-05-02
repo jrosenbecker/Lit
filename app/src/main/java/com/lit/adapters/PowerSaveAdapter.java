24b6ac0b33a5987e624a885502101be2de0770a5
jrosenbecker <rosenbeckerjw>
2016-05-02 16:47:24 -0500

    Deleted Unnecessary comments

--- app/src/main/java/com/lit/adapters/PowerSaveAdapter.java
+++ app/src/main/java/com/lit/adapters/PowerSaveAdapter.java
@@ -114,36 +114,4 @@ public class PowerSaveAdapter extends BaseExpandableListAdapter {
     public boolean isChildSelectable(int i, int i1) {
         return true;
     }
-
-
-//    @Override
-//    public int getCount() {
-//        return powerSaveList.size();
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        return powerSaveList.get(position);
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return powerSaveList.get(position).getId();
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-//        convertView = inflater.inflate(R.layout.power_save_single_line, parent, false);
-//        // TODO: Set up all of the views in the single line
-//        TextView lightName = (TextView) convertView.findViewById(R.id.power_save_light_name);
-//        TextView activeSensor = (TextView) convertView.findViewById(R.id.power_save_active_sensor);
-//        Light light = powerSaveList.get(position);
-//        lightName.setText(light.getLightName());
-////        activeSensor.setText(light.getActiveSensor());
-//
-//
-//        return convertView;
-//
-//    }
 }
